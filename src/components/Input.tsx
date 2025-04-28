0073c0fc3c28ef04c9b58413d59591d442e7bae5
RogerPatriota <rogerpatriota@gmail.com>
2025-04-27 22:07:14 -0300

    core: upate input text

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,28 @@
 import React from "react";
+import {  FormItem  } from 'azure-devops-ui/FormItem'
+import { TextField, TextFieldWidth } from 'azure-devops-ui/TextField'
 
 export function Input(props: any) {
     return (
-        <div className="w-[47%]">
-            <div className="label">
-                <span className="label-text">{props.title}</span>
-            </div>
-            <input type="text" placeholder="Type here" className="h-12 pl-3 rounded-lg border-gray-400 border-2 w-full" />
-            <div className="label">
-                <span className="label-text-alt text-gray-500">{props.description}</span>
-            </div>
-        </div>
+        <FormItem
+            className="w-[47%] mb-6"
+            label={props.title} 
+            message={props.description}>
+            <TextField 
+                className=""
+                multiline
+                width={TextFieldWidth.auto}
+            />
+        </FormItem>
+        // <div className="w-[47%]">
+        //     <div className="label">
+        //         <span className="label-text">{props.title}</span>
+        //     </div>
+        //     <input type="text" placeholder="Type here" className="h-12 pl-3 rounded-lg border-gray-400 border-2 w-full" />
+        //     <div className="label">
+        //         <span className="label-text-alt text-gray-500">{props.description}</span>
+        //     </div>
+        // </div>
     )
 }
 
